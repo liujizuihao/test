25bb151c30173fd50c2aa0d1b96cda3365ecf311
zhaoxiaoyang <zhaoxiaoyang@nuoyuan.com.cn>
2018-07-23 18:18:26 +0800

    no message

--- testProject/src/com/test/aa/TestProject.java
+++ testProject/src/com/test/aa/TestProject.java
@@ -3,10 +3,7 @@ package com.test.aa;
 public class TestProject {
 
 	public static void main(String[] args) {
-		System.out.println("好好学习");
-		System.out.println("好好学习-User1");
-		//修改User2
-		System.out.println("好好学习-User1 第二次");
-		System.out.println("User1又改了");
+		String str = "鎴戠殑鐢佃剳";
+		String str1 = "鐢佃剳1";
 	}
 }
